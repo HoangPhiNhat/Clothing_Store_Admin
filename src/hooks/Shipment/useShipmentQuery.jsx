2e9d6f5f210089b9616602a79ba37132d6cd057e
Lucas-0911 <phuclq0911@gmail.com>
2024-11-24 13:35:51 +0700

    Fix(courier): fix pagination on courier profile for shipment

--- src/hooks/Shipment/useShipmentQuery.jsx
+++ src/hooks/Shipment/useShipmentQuery.jsx
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getShipmentByCourierId } from "../../services/shipment";
 
 const useShipmentQuery = (action, id, page) => {
-  let queryKey = id ? ["SHIPMENT_KEY", id] : ["SHIPMENT_KEY", page];
+  let queryKey = page ? ["SHIPMENT_KEY", page] : ["SHIPMENT_KEY", id];
 
   const { data, ...rest } = useQuery({
     queryKey,
     queryFn: async () => {
       switch (action) {
         case "GET_SHIPMENT_BY_COURIER_ID":
-          return await getShipmentByCourierId(id,page);
+          return await getShipmentByCourierId(id, page);
         default:
           return null;
       }
